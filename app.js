1a1095e756dcc51739ee695cc39ebc19e26b5918
nishukundar <nischithasrinivas83@gmail.com>
2024-05-04 11:21:24 +0530

    score updated

--- app.js
+++ app.js
@@ -4,15 +4,18 @@ let compScore = 0;
 const choices = document.querySelectorAll(".choice");
 const msg = document.querySelector("#msg");
 
+const userScorePara = document.querySelector("#user-score");
+const compScorePara = document.querySelector("#comp-score");
+
+
 const genCompChoice = () => {
     const options = [ "rock", "paper", "scissors"];
-    const randIdx= Math.floor(Math.random()*3);
+    const randIdx= Math.floor(Math.random()*3); 
     return options[randIdx];
 
 };
 
 const drawGame = () => {
-    console.log("Game was draw.");
     msg.innerText = "Game was draw. Play again";
     msg.style.backgroundColor = "rgb(8, 8, 83)";
 
@@ -20,12 +23,15 @@ const drawGame = () => {
 
 const showWinner = (userWin, userChoice, compChoice) => {
     if(userWin) {
-        console.log("YOU WIN :)");
+        userScore++;
+        userScorePara.innerText = userScore;
         msg.innerText = `Hurray! You win. Your ${userChoice} beats ${compChoice}`;
         msg.style.backgroundColor = "green";
     }
     else{
-        console.log("YOU LOSE :(");
+        compScore++;
+        compScorePara.innerText = compScore;
+        
         msg.innerText = msg.innerText = `Oh no :( You lost. ${compChoice} beats your ${userChoice}`;
         msg.style.backgroundColor = "red";
     }
